e63ce4f2ee0deba5f2b44fb063c59760907f04bc
941221 <626607098@qq.com>
2017-07-20 09:30:32 +0800

    q

--- js/L-pic-2.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	let btn=document.querySelector('.welldown');
-	btn.addEventListener('touchend',function(e){
-		e.preventDefault();
-		$('.shadow').css('display','block');
-		location.href='L-head-2.html'
-	})
-})
